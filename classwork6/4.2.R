de6f694df4888dc30103d2b085e51046a003c318
Zdrast <e_gor-97@mail.ru>
2018-10-29 17:29:41 +0500

    Presentation add

--- classwork6/4.2.R
+++ classwork6/4.2.R
@@ -61,3 +61,87 @@ msleep %>%
             min_sleep = min(sleep_total), 
             max_sleep = max(sleep_total),
             total = n())
+
+tbl_df(flights)
+
+glimpse(flights)
+
+flights %>%
+  filter(dest == "MSN")
+
+flights %>%
+  filter(dest == "MSN", month == 1, day <= 7)
+
+flights %>%
+  arrange(origin, year, month, day)
+
+flights %>%
+  filter(dest == "MSN") %>%
+  arrange(desc(dep_delay))
+
+flights %>%
+  select(origin, year, month, day)
+
+flights %>%
+  select(origin, year:day, starts_with("dep"))
+
+flights %>%
+  select(-dest, -starts_with("arr"),
+         -ends_with("time"))
+
+flights %>%
+  rename(y = year, m = month, d = day)
+
+flights %>%
+  mutate(
+    gain = arr_delay - dep_delay,
+    speed = (distance / air_time) * 60,
+    gain_per_hour = gain / (air_time / 60)) %>%
+  select(gain:gain_per_hour)
+
+aggregate(dep_delay ~ month, flights, mean,
+          subset = flights$dest == "MSN")
+
+msn_by_month <- flights %>%
+  filter(dest == "MSN") %>%
+  group_by(month)
+flights
+
+msn_by_month %>%
+  summarise(
+    flights = n(),
+    avg_delay = mean(dep_delay, na.rm = TRUE),
+    n_planes = n_distinct(tailnum))
+
+flights %>%
+  group_by(dest, month) %>%
+  tally
+
+msn_by_month %>% ungroup
+
+per_day <- flights %>%
+  group_by(dest, year, month, day) %>%
+  summarise(flights = n())
+per_day
+
+per_month <- per_day %>%
+  summarise(flights = sum(flights))
+per_month
+
+per_year <- per_month %>%
+  summarise(flights = sum(flights))
+per_year
+
+flights %>%
+  group_by(dest, month) %>%
+  mutate(timely = row_number(dep_delay),
+         late = row_number(desc(dep_delay))) %>%
+  select(dep_delay, timely:late)
+
+mean_center <- function(xs) {
+  xs - mean(xs, na.rm = TRUE)
+}
+flights %>%
+  group_by(dest, month) %>%
+  mutate(c_delay = mean_center(dep_delay)) %>%
+  select(dep_delay, c_delay)
